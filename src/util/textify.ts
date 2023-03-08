c155676f946e9476329e6aaeff12c4c23c3fa2e5
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-03-08 17:16:36 +0800

    Refactor textify

--- src/util/textify.ts
+++ src/util/textify.ts
@@ -1,20 +1,18 @@
 import { toText } from 'hast-util-to-text'
-import { Parent } from './element'
+import { Content, isParent, Node, Parent } from './element'
 
 export interface TextifyOptions {
   trim?: boolean
   multiLine?: boolean
 }
 
-export function textify(tree: Parent, options: Partial<TextifyOptions> = {}) {
+export function textify(node?: Node | null, options: Partial<TextifyOptions> = {}): string {
+  if (!node) { return '' }
+  const root: Parent = isParent(node) ? node : { type: 'root', children: [node as Content] }
   const { trim, multiLine }: TextifyOptions = { trim: true, multiLine: false, ...options }
-  let text = toText(tree)
+  let text = toText(root)
   text = text.replace(/ ?\n+ ?\n*/g, '\n')
   if (!multiLine) { text = text.replace(/\n/g, ' ') }
   if (trim) { text = text.replace(/ +/g, ' ').trim() }
   return text
-}
-
-export function textifyChildren({ children }: Parent) {
-  return textify({ type: 'root', children })
 }
