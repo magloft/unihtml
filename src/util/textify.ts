671132e1e1dacda0045164d3dd04ddc28f33ebc6
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-06-02 11:36:30 +0800

    remove unist package

--- src/util/textify.ts
+++ src/util/textify.ts
@@ -10,7 +10,7 @@ export function textify(node?: Node | null, options: Partial<TextifyOptions> = {
   if (!node) { return '' }
   const root: Parent = isParent(node) ? node : { type: 'root', children: [node as Content] }
   const { trim, multiLine }: TextifyOptions = { trim: true, multiLine: false, ...options }
-  let text = toText(root)
+  let text = toText(root as any)
   text = text.replace(/ ?\n+ ?\n*/g, '\n')
   if (!multiLine) { text = text.replace(/\n/g, ' ') }
   if (trim) { text = text.replace(/ +/g, ' ').trim() }
